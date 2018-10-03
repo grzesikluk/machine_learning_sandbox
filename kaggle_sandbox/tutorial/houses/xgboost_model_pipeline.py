27578faa20cdaf758a87f5b03abdd800da2e1607
Lukasz Grzesik <lgrzesik@akamai.com>
2018-10-03 15:09:47 +0200

    Added pipeline example

--- kaggle_sandbox/tutorial/houses/xgboost_model_pipeline.py
+++ kaggle_sandbox/tutorial/houses/xgboost_model_pipeline.py
@@ -1,15 +1,13 @@
 import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.impute import SimpleImputer
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import Imputer
 from xgboost import XGBRegressor
 
 FILE = './melb_data.csv'
-
-
-def getMissingCols(data):
-    return [col for col in data.columns
-            if data[col].isnull().any()]
 
 
 def getMelbPredictors(data):
@@ -26,26 +24,14 @@ def getTrainAndTestData(data, target):
     return (X_train, y_train, X_test, y_test)
 
 
-def getImputedTrainTestData(input):
-    my_imputer = SimpleImputer()
-    return my_imputer.fit_transform(input[0]), my_imputer.transform(input[2])
-
-
-def score_dataset_xgboost(X_train, X_test, y_train, y_test, estimators=2000, learning_rate=0.1):
-    model = XGBRegressor(n_estimators=estimators, learning_rate=learning_rate, n_jobs=8)
-    model.fit(X_train, y_train, verbose=False, early_stopping_rounds=15, eval_set=[(X_test, y_test)])
-    predictions = model.predict(X_test)
-
-    return mean_absolute_error(predictions, y_test)
-
-
 if __name__ == "__main__":
-    # load data
     melbourne_data = pd.read_csv(FILE)
     melb_numeric_predictors = getMelbPredictors(melbourne_data)
     test_input = getTrainAndTestData(melb_numeric_predictors, melbourne_data.Price)
-    missing_cols = getMissingCols(test_input[0])
 
-    X_train, X_test = getImputedTrainTestData(test_input)
+    pipeline = make_pipeline(Imputer(), RandomForestRegressor())
+    pipeline.fit(test_input[0],test_input[1])
+    predictions = pipeline.predict(test_input[2])
+    print(mean_absolute_error(predictions, test_input[3]))
 
-    print(score_dataset_xgboost(X_train, X_test, test_input[1], test_input[3]))
+
